77a4aacfd158cfa1eaebc2173341e2c769954021
Fadlian17 <alfansyahfadlian@gmail.com>
2020-06-29 22:49:13 +0700

    try to fix task1 week4

--- qtdesigner_survey.py
+++ qtdesigner_survey.py
@@ -1,50 +1,89 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QLineEdit, QStackedLayout, QStackedWidget, QVBoxLayout
+from PyQt5.QtWidgets import QStackedWidget, QApplication, QMainWindow, QWidget, QLineEdit, QStackedLayout, QStackedWidget, QVBoxLayout
 from PyQt5 import uic
-import survey
+# import survey
 import sys
+import json
 import qtmodern.styles
 import qtmodern.windows
 
 
-class App(QWidget):
+class loginSurvey(QWidget):
     def __init__(self):
-        super(App, self).__init__()
+        super(loginSurvey, self).__init__()
+        self.mainUI()
+        self.setFixedSize(500, 200)
+
+    def mainUI(self):
         uic.loadUi("loginSurvey.ui", self)
-        self.pushButton.clicked.connect(self.changeButton)
 
 
-def changeButton(self):
-    # username = self.usernameEdit.text()
-    username2 = self.findChild(QLineEdit, "usernameEdit")
-    print(username2.text())
-
-
-class App2(QWidget):
+class formSurvey(QWidget):
     def __init__(self):
-        super(App2, self).__init__()
-        uic.loadUi("formSurvey.ui", self)
+        super(formSurvey, self).__init__()
+        self.mainUI()
+        self.setFixedSize(500, 200)
+
+    def mainUI(self):
+        uic.loadUi("form.ui", self)
+
+
+# def changeButton(self):
+#     # username = self.usernameEdit.text()
+#     username2 = self.findChild(QLineEdit, "usernameEdit")
+#     print(username2.text())
+
+
+# class App2(QWidget):
+#     def __init__(self):
+#         super(App2, self).__init__()
+#         uic.loadUi("formSurvey.ui", self)
 
 
 class MainApp(QMainWindow):
     def __init__(self):
         super(MainApp, self).__init__()
-        self.first = App()
-        self.second = App2()
-        self.verticalLayout = QVBoxLayout()
-        self.verticalLayout.addWidget(self.first)
-        self.verticalLayout.addWidget(self.first)
+        self.mainUI()
 
-        self.widget = QWidget()
-        self.widget.setLayout(self.verticalLayout)
+    def mainUI(self):
+        self.lSurvey = loginSurvey()
+        self.fSurvey = formSurvey()
 
-        self.setCentralWidget(self.widget)
+        # stacked widget
+        self.stackLayout = QStackedLayout()
+        self.stackLayout.addWidget(self.lSurvey)
+        self.stackLayout.addWidget(self.fSurvey)
+
+    def mainLayout(self):
+        self.layoutWidget = QVBoxLayout()
+        self.layoutWidget.addWidget(self.stackLayout)
+        self.setLayout(self.layoutWidget)
+
+    def validation(self):
+        valid_data = self.getValue()[0]['user']
+        username = self.lSurvey.username.text()
+        password = self.lSurvey.password.text()
+
+        if username == "" or password == "":
+            print("Username or Password not empty")
+        else:
+            if username == valid_data['username'] and password == valid_data['password']:
+                self.stackLayout.setCurrentIndex(1)
+            else:
+                print("username or Password not valid!")
+
+    def action_valid(self):
+        self.lSurvey.pushButton.clicked.connect(self.validation)
+
+    def getValue(self):
+        value = open("datas.json")
+        value = json.loads(value)
+        return value
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    win = App()
-    # win.show()
-
+    win = MainApp()
+    win.show()
     qtmodern.styles.dark(app)
     win_new = qtmodern.windows.ModernWindow(win)
     win_new.show()

f5329eca9aef95a4c546bb450f9f9cfe30afbd28
Fadlian17 <alfansyahfadlian@gmail.com>
2020-07-01 19:58:24 +0700

    fixed task1 and task2 week4

--- qtdesigner_survey.py
+++ qtdesigner_survey.py
@@ -2,7 +2,10 @@ import json
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5.QtPrintSupport import QPrintDialog, QPrinter, QAbstractPrintDialog
-from PyQt5.QtChart import QtCharts
+from PyQt5.QtChart import *
+from PyQt5.Qt import Qt
+from PyQt5.QtGui import QIcon
+import requests
 
 
 class loginSurvey(QWidget):
@@ -39,12 +42,15 @@ class Result(QWidget):
         uic.loadUi("report.ui", self)
 
 
-class App(QWidget):
+class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
         self.mainUI()
         self.mainLayout()
         self.action()
+        self.toolBarsSurvey()
+        self.addToolBar(self.toolBarHome)
+        self.addToolBar(self.toolBarForm)
 
     def mainUI(self):
         self.login = loginSurvey()
@@ -54,14 +60,66 @@ class App(QWidget):
         self.stackedLayout.addWidget(self.login)
         self.stackedLayout.addWidget(self.form)
         self.stackedLayout.addWidget(self.result)
+        self.stackedLayout.addWidget(self.chart())
         self.favoriteItem = ""
 
         self.form.comboBox1.activated.connect(self.valueCombo1)
 
     def mainLayout(self):
-        self.layout = QVBoxLayout()
-        self.layout.addLayout(self.stackedLayout)
-        self.setLayout(self.layout)
+        self.layout = QWidget()
+        self.layout.setLayout(self.stackedLayout)
+        self.setCentralWidget(self.layout)
+
+    def toolBarsSurvey(self):
+        self.toolBarForm = QToolBar()
+        buttonToolbar = QAction(QIcon("icon/market.png"), "test", self)
+        self.toolBarForm.addAction(buttonToolbar)
+        buttonToolbar.triggered.connect(self.tab_chart)
+
+        self.toolBarHome = QToolBar()
+        buttonToolbar = QAction(QIcon("icon/person.png"), "test", self)
+        self.toolBarHome.addAction(buttonToolbar)
+        buttonToolbar.triggered.connect(self.tab_home)
+
+    def tab_chart(self):
+        self.stackedLayout.setCurrentIndex(3)
+
+    def tab_home(self):
+        self.stackedLayout.setCurrentIndex(0)
+
+    def getDataJson(self):
+        with open('survey.json', 'r') as respon:
+            data = json.load(respon)
+        return data
+
+    def chart(self):
+        respon = requests.get(
+            "https://res.cloudinary.com/sivadass/raw/upload/v1535817394/json/products.json")
+        json = respon.json()
+
+        harga = list(map(lambda a: a['price'], json))
+        barset0 = QBarSet("Harga Buah")
+        for i in harga:
+            barset0.append(i)
+
+        series = QBarSeries()
+        series.append(barset0)
+
+        chart = QChart()
+        chart.addSeries(series)
+        chart.setAnimationOptions(QChart.SeriesAnimations)
+
+        name = list(map(lambda a: a['name'], json))
+        axisX = QBarCategoryAxis()
+        axisX.append(name)
+
+        axisY = QValueAxis()
+        axisY.setRange(0, 950)
+
+        chart.addAxis(axisX, Qt.AlignBottom)
+        chart.addAxis(axisY, Qt.AlignLeft)
+        chartView = QChartView(chart)
+        return chartView
 
     def valueCombo1(self, index):
         self.favoriteItem = self.form.comboBox1.itemText(index)
@@ -126,42 +184,76 @@ class App(QWidget):
         saranMasukan = self.result.findChild(QLabel, "label_32")
         saranMasukan.setText(data["saran_masukan"])
 
+    # show to login page
+
+    def act_login(self):
+        self.stackedLayout.setCurrentIndex(1)
+
+    # show to form page
+
     def act_form(self):
         self.resultsResponse()
         self.stackedLayout.setCurrentIndex(2)
-
-    def act_login(self):
-
-        self.stackedLayout.setCurrentIndex(1)
-
-    def action(self):
-        self.login.pushButton.clicked.connect(self.act_login)
-        self.form.pushButton.clicked.connect(self.act_form)
-        self.result.pushButton.clicked.connect(self.addToJson)
-        self.result.pushButton_2.clicked.connect(self.printPdf)
 
     def addToJson(self):
         data = self.values()
         toJson = json.dumps(data, indent=4)
         fwrite = open('survey.json', 'w')
         fwrite.write(toJson)
-        QMessageBox.information(self, "About", "Export to Json Success")
+        QMessageBox.information(self, "About", "Survey Berhasil Di Export")
 
-    def printToPdf(self):
-        self.printPdf = QPrinter()
-        self.dialog = QPrintDialog(self.printPdf)
-        if self.dialog.exec_() == QPrintDialog.accepted:
-            self.result.document().print_(self.printPdf)
+    # pdf print
+    def getToPrint(self):
+        data = self.values()
+        text = """
+                    Hasil Survey Supermarket 
 
-    def getData(self):
-        data = open("datas.json")
-        data = json.load(data)
-        return data
+            Item Favorite: {}
+            Kelengkapan Stok: {}
+            Toilet: {}
+            Rak Makanan: {}
+            Rak Buah: {}
+            Rak Susun: {}
+            Refrigator: {}
+            Lantai Toko: {}
+            Lokasi Strategis: {}
+            Lahan Parkir: {}
+            Saran Masukan Anda :{}
+
+            Semoga Harimu Menyenangkan 
+        """.format(data["favorite_item"], data["kelengkapan_stok"], data["toilet"], data['rak_makanan'], data["rak_buah"], data["rak_pecah_belah"], data["refrigator"], data["lantai_toko"], data["lokasi_strategis"], data["lahan_parkir"], data["saran_masukan"])
+        self.textArea = QTextEdit()
+        self.textArea.setText(text)
+        self.button = QPushButton("Print")
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.textArea)
+        self.layout.addWidget(self.button)
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+        self.setCentralWidget(self.widget)
+        self.button.clicked.connect(self.printTo)
+
+    # print dialog
+    def printTo(self):
+        self.printer = QPrinter()
+        self.dialog = QPrintDialog(self.printer)
+        if self.dialog.exec_() == QDialog.Accepted:
+            self.textArea.document().print_(self.dialog.printer())
+
+    def action(self):
+        self.username = self.login.findChild(QLineEdit, "lineEdit1")
+        self.password = self.login.findChild(QLineEdit, "lineEdit_2")
+        self.login.pushButton.clicked.connect(self.act_login)
+        self.form.pushButton.clicked.connect(self.act_form)
+        self.result.pushButton.clicked.connect(self.addToJson)
+        btnPrint = self.result.findChild(QPushButton, "pushButton_2")
+        btnPrint.clicked.connect(self.getToPrint)
 
 
 if __name__ == "__main__":
     app = QApplication([])
     window = App()
     window.show()
-    window.setFixedSize(800, 650)
+    window.setWindowTitle("Survey Market")
+    window.resize(1200, 800)
     app.exec_()
